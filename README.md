145deb46bd240f7a0661c999018755c5a6f43db2
fcebron <fcebronATfree.fr>
2018-04-28 19:41:29 +0200

    first steps: updating packages and setting up softwares

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Auto-Install-Scripts
 Script for automating softwares installation on Ubuntu
+
+## Installation
+### First step
+Install git: sudo apt install git
+
+### Second step
+Clone this repository: git clone https://github.com/fcebron/Auto-Install-Scripts.git
+
+### Third step
+Launch the script in super-user mode: sudo ./autoInstall.sh
--- autoInstall.sh
+++ autoInstall.sh
@@ -1,16 +1,26 @@
 #! /bin/bash
 
-# Fetch updates:
-echo "=> Fetching updates"
-apt update
+# Adding Canonical's partner repository:
+sed -i.bak "/^# deb .*partner/ s/^# //" /etc/apt/sources.list
 
-# Install aptitude for futures installations:
-echo "=> Installing Aptitude"
-apt install -y aptitude
+# Checking if aptitude is already present in the system:
+if hash aptitude 2>/dev/null; then
+    echo "aptitude is already installed"
+else
+    # Install aptitude for futures installations:
+    echo "aptitude is not installed!"
+    echo "=> Installing Aptitude"
+    apt update
+    apt install -y aptitude
+fi
 
 # Update and Upgrade system:
 echo "Updating full system"
 aptitude update
+# Upgrading packages:
+#aptitude safe-upgrade -y
+# Upgrading packages and distrib:
+#aptitude dist-upgrade -y
 aptitude upgrade -y
 
 echo "Completed !"
