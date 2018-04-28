6e509fd8f970257ae1716edf9b07ac096c4712a4
fcebron <fcebronATfree.fr>
2018-04-28 21:05:33 +0200

    adding colors

--- autoInstall.sh
+++ autoInstall.sh
@@ -1,26 +1,28 @@
 #! /bin/bash
 
+# CYAN_U=\e[4m\e[36m
+# GREEN_B=\e[1m\e[32m
+# RED_B=\e[1m\e[31m
+# DEFAULT=\e[0m
+
 # Adding Canonical's partner repository:
+echo -e "\e[1m\e[32m==> \e[1m\e[37mAdding Canonical's partner repository\e[0m"
 sed -i.bak "/^# deb .*partner/ s/^# //" /etc/apt/sources.list
 
 # Checking if aptitude is already present in the system:
 if hash aptitude 2>/dev/null; then
-    echo "aptitude is already installed"
+    echo -e "\n\e[1m\e[36m  --> aptitude is already installed\e[0m"
 else
     # Install aptitude for futures installations:
-    echo "aptitude is not installed!"
-    echo "=> Installing Aptitude"
+    echo -e "\n\e[1m\e[31m  --> aptitude is not installed!\e[0m"
+    echo -e "\n\e[1m\e[32m==> \e[1m\e[37mInstalling Aptitude\e[0m"
     apt update
     apt install -y aptitude
 fi
 
 # Update and Upgrade system:
-echo "Updating full system"
+echo -e "\n\e[1m\e[32m==> \e[1m\e[37mUpdating full system\e[0m"
 aptitude update
-# Upgrading packages:
-#aptitude safe-upgrade -y
-# Upgrading packages and distrib:
-#aptitude dist-upgrade -y
-aptitude upgrade -y
+aptitude dist-upgrade -y
 
-echo "Completed !"
+echo -e "\n\e[1m\e[32m==> \e[1m\e[37mEnd of the script!\e[0m\n"
